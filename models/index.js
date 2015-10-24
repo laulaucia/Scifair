7cf2fba9977f71ad3cef4792dae2f59904c600d5
Laulaucia <buitrago.laura@gmail.com>
2015-10-24 10:12:42 -0700

    added things to scripts.js to add in map

--- models/index.js
+++ models/index.js
@@ -1,10 +1,3 @@
  mongoose.connect( process.env.MONGOLAB_URI ||
                       process.env.MONGOHQ_URL || 
                       'mongodb://localhost/scifair' );
- $(document).ready(function(){
-
- map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: 37.783, lng: -122.4167},
-    	zoom: 3
-  	});
-});
---
+++ public/scripts/scripts.js
@@ -0,0 +1,8 @@
+var map;
+ $(document).ready(function(){
+
+ map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 37.783, lng: -122.4167},
+    	zoom: 3
+  	});
+});
