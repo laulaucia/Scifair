c84deb8baad37e7b5d2f546a9d80db1b0ca1b532
Laulaucia <buitrago.laura@gmail.com>
2015-10-26 15:04:23 -0700

    added map to index file

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,15 @@
 var map;
  $(document).ready(function(){
 
+ function initMap(){
  map = new google.maps.Map(document.getElementById('map'), {
     	center: {lat: 37.783, lng: -122.4167},
     	zoom: 3
   	});
+}
 });
+
+ initMap();
 
 
 // /* #####################################################################
